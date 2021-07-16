88550c5724fcc81389e05b453cb77a9c8c6e00c0
eahomesys <eahomesys@gmail.com>
2021-07-16 16:35:07 -0600

    Deduplicate group view functions

--- pysa_db/Controllers/GroupTableViewController.swift
+++ pysa_db/Controllers/GroupTableViewController.swift
@@ -11,9 +11,6 @@ import Firebase
 import FirebaseStorage
 
 class GroupTableViewController: UIViewController {
-
-  
-    
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -33,48 +30,64 @@ class GroupTableViewController: UIViewController {
 
     var filter = GroupTableViewController.GlobalVariable.myString
     //var filter = "MissionLocation"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("filter: \(filter)")
         title = filter // need to change the title to more verbose version
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        let collection = db.collection(K.FStore.collectionName)
+        
         if (filter == "Calling") {
             title = "Leadership"
-            orgLeadership()
+            let query = collection.whereField(K.FStore.Calling, in: ["President", "First Counselor", "Second Counselor", "Secretary"]).order(by: "Org")
+            loadMembers(query: query)
         }
         else if (filter == "Address")  {
             title = "Apartments"
-            apartmentList()
+            let query = collection.order(by: "Address").order(by: "Apt_no")
+            loadMembers(query: query)
         }
-        else {
-            // need to change the title to more verbose version
-            if(filter == "Major"){
+        else if(filter == "Major"){
                 title = "Field of Study"
-            }
-            else if(filter == "SecondLang"){
-                title = "Languages"
-            }
-            else if(filter == "MissionLocation"){
-                title = "Missions"
-            }
-            else if(filter == "HEGroup"){
-                title = "Home Evening Groups"
-            }
-            else if (filter == "Org") {
-                title = "EQ-RS Membership"
-            }
-            else if (filter == "Committee") {
-                title = "Committees"
-            }
-            loadMessages()
+                let query = collection.whereField(K.FStore.Status, isEqualTo: "Current").order(by: filter)
+            loadMembers(query: query)
         }
+        else if(filter == "SecondLang"){
+            title = "Languages"
+            let query = collection.whereField(K.FStore.Status, isEqualTo: "Current").order(by: filter)
+            loadMembers(query: query)
+        }
+        else if(filter == "MissionLocation"){
+            title = "Missions"
+            let query = collection.whereField(K.FStore.Status, isEqualTo: "Current").order(by: filter)
+            loadMembers(query: query)
+        }
+        else if(filter == "HEGroup"){
+            title = "Home Evening Groups"
+            let query = collection.whereField(K.FStore.Status, isEqualTo: "Current").order(by: filter)
+            loadMembers(query: query)
+        }
+        else if (filter == "Org") {
+            title = "EQ-RS Membership"
+            let query = collection.whereField(K.FStore.Status, isEqualTo: "Current").order(by: filter)
+            loadMembers(query: query)
+        }
+        else if (filter == "Committee") {
+            title = "Committees"
+            let query = collection.whereField(K.FStore.Status, isEqualTo: "Current").order(by: filter)
+            loadMembers(query: query)
+        }
+        
     }
+
     
 
-    func loadMessages() {
-        //db.collection(K.FStore.collectionName).order(by: filter).addSnapshotListener { [self] querySnapshot, error in
-        db.collection(K.FStore.collectionName).whereField(K.FStore.Status, isEqualTo: "Current").order(by: filter).addSnapshotListener { [self] querySnapshot, error in
+    func loadMembers(query: Query) {
+
+        query.addSnapshotListener { [self] querySnapshot, error in
             self.miss_lang = []
             if let e = error {
                 print("There was an issue retreiving data from Firestore, \(e)")
@@ -105,11 +118,8 @@ class GroupTableViewController: UIViewController {
                                 let indexPath = IndexPath(row: 0, section: 0)
                                 self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
-                            
-                            
-                            
+ 
                         }
-                        
                     }
                     for member in miss_lang {
                         if(filter == "MissionLocation"){
@@ -124,11 +134,17 @@ class GroupTableViewController: UIViewController {
                         else if(filter == "Org") {
                             section.append(member.Org)
                         }
+                        else if(filter == "Calling") {
+                            section.append(member.Org)
+                        }
                         else if(filter == "Committee") {
                             section.append(member.Committee)
                         }
                         else if(filter == "HEGroup") {
                             section.append(member.HEGroup)
+                        }
+                        else if(filter == "Address") {
+                            section.append(member.Address)
                         }
                     }
 
@@ -161,6 +177,11 @@ class GroupTableViewController: UIViewController {
                                     rows.append(miss_lang[num1])
                                 }
                             }
+                            else if(filter == "Calling"){
+                                if(String(miss_lang[num1].Org) == String(section[num2])){
+                                    rows.append(miss_lang[num1])
+                                }
+                            }
                             else if(filter == "Committee"){
                                 if(String(miss_lang[num1].Committee) == String(section[num2])){
                                     rows.append(miss_lang[num1])
@@ -171,164 +192,21 @@ class GroupTableViewController: UIViewController {
                                     rows.append(miss_lang[num1])
                                 }
                             }
+                            else if(filter == "Address"){
+                                if(String(miss_lang[num1].Address) == String(section[num2])){
+                                    rows.append(miss_lang[num1])
+                                }
+                            }
                             
                             
                         }
                         items.append(rows)
                         rows = []
-
                     }
-
                 }
             }
         } // end closure
-        // ********* section ***************************************
-        
-        
     }
-    
-    func orgLeadership() {
-        //db.collection(K.FStore.collectionName).order(by: filter).addSnapshotListener { [self] querySnapshot, error in
-        db.collection(K.FStore.collectionName)
-            .whereField(K.FStore.Calling, in: ["President", "First Counselor", "Second Counselor", "Secretary"])
-            .order(by: "Org").addSnapshotListener { [self] querySnapshot, error in
-            self.miss_lang = []
-            if let e = error {
-                print("There was an issue retreiving data from Firestore, \(e)")
-            } else {
-                if let snapshotDocuments = querySnapshot?.documents {
-                    for doc in snapshotDocuments {
-                        let data = doc.data()
-                        if let LastName = data[K.FStore.lastNameField] as? String,
-                           let FirstName = data[K.FStore.firstNameField] as? String,
-                           let Address = data[K.FStore.addressField] as? String,
-                           let Apt_no = data[K.FStore.aptField] as? String,
-                           let PictureURL = data[K.FStore.pictureURL] as? String,
-                           let MissionLocation = data[K.FStore.MissionLocation] as? String,
-                           let SecondLang = data[K.FStore.SecondLang] as? String,
-                           let Major = data[K.FStore.Major] as? String,
-                           let Org = data[K.FStore.Org] as? String,
-                           let Committee = data[K.FStore.Committee] as? String,
-                           let Calling = data[K.FStore.Calling] as? String,
-                           let HEGroup = data[K.FStore.HEGroup] as? String
-                        
-                           {
-                            let newMiss_Lang = MissLangModel(FirstName: FirstName, LastName: LastName, Address: Address, Apt_no: Apt_no, PictureURL: PictureURL, MissionLocation: MissionLocation, SecondLang: SecondLang, Major: Major, Org: Org, Committee: Committee, Calling: Calling, HEGroup: HEGroup)
-                            //print(newMiss_Lang)
-                            self.miss_lang.append(newMiss_Lang)
-                            
-                            DispatchQueue.main.async {
-                                self.tableView.reloadData()
-                                let indexPath = IndexPath(row: 0, section: 0)
-                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
-                            }
-                            
-                            
-                            
-                        }
-                        
-                    }
-                    for member in miss_lang {
-                        section.append(member.Org)
-                    }
-
-                    section = section.removingDuplicates()
-                    
-                    let index1 = 0...miss_lang.count - 1
-                    let index2 = 0...section.count - 1
-                    var rows: [MissLangModel] = []
-
-                    for num2 in index2 {
-                        for num1 in index1 {
-                            if(String(miss_lang[num1].Org) == String(section[num2])){
-                                rows.append(miss_lang[num1])
-                            }
-                        }
-                        items.append(rows)
-                        rows = []
-
-                    }
-
-                }
-            }
-        } // end closure
-        // ********* section ***************************************
-        
-        
-    }
-    
-    func apartmentList() {
-        //db.collection(K.FStore.collectionName).order(by: filter).addSnapshotListener { [self] querySnapshot, error in
-        db.collection(K.FStore.collectionName)
-            .order(by: "Address")
-            .order(by: "Apt_no").addSnapshotListener { [self] querySnapshot, error in
-            self.miss_lang = []
-            if let e = error {
-                print("There was an issue retreiving data from Firestore, \(e)")
-            } else {
-                if let snapshotDocuments = querySnapshot?.documents {
-                    for doc in snapshotDocuments {
-                        let data = doc.data()
-                        if let LastName = data[K.FStore.lastNameField] as? String,
-                           let FirstName = data[K.FStore.firstNameField] as? String,
-                           let Address = data[K.FStore.addressField] as? String,
-                           let Apt_no = data[K.FStore.aptField] as? String,
-                           let PictureURL = data[K.FStore.pictureURL] as? String,
-                           let MissionLocation = data[K.FStore.MissionLocation] as? String,
-                           let SecondLang = data[K.FStore.SecondLang] as? String,
-                           let Major = data[K.FStore.Major] as? String,
-                           let Org = data[K.FStore.Org] as? String,
-                           let Committee = data[K.FStore.Committee] as? String,
-                           let Calling = data[K.FStore.Calling] as? String,
-                           let HEGroup = data[K.FStore.HEGroup] as? String
-                        
-                           {
-                            let newMiss_Lang = MissLangModel(FirstName: FirstName, LastName: LastName, Address: Address, Apt_no: Apt_no, PictureURL: PictureURL, MissionLocation: MissionLocation, SecondLang: SecondLang, Major: Major, Org: Org, Committee: Committee, Calling: Calling, HEGroup: HEGroup)
-                            //print(newMiss_Lang)
-                            self.miss_lang.append(newMiss_Lang)
-                            
-                            DispatchQueue.main.async {
-                                self.tableView.reloadData()
-                                let indexPath = IndexPath(row: 0, section: 0)
-                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
-                            }
-                            
-                            
-                            
-                        }
-                        
-                    }
-                    for member in miss_lang {
-                        section.append(member.Address)
-                    }
-
-                    section = section.removingDuplicates()
-                    
-                    print("section: \(section)")
-                    
-                    let index1 = 0...miss_lang.count - 1
-                    let index2 = 0...section.count - 1
-                    var rows: [MissLangModel] = []
-
-                    for num2 in index2 {
-                        for num1 in index1 {
-                            if(String(miss_lang[num1].Address) == String(section[num2])){
-                                rows.append(miss_lang[num1])
-                            }
-                        }
-                        items.append(rows)
-                        rows = []
-
-                    }
-                    
-                }
-            }
-        } // end closure
-        // ********* section ***************************************
-        
-        print(items)
-    }
-    
 }
 
 
@@ -367,8 +245,9 @@ extension GroupTableViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         //print("indexPath.section: \(indexPath.section)")
         //print("indexPath.row: \(indexPath.row)")
-            cell.imageView?.image = image
-            cell.textLabel?.textColor = .black
+        cell.imageView?.image = image
+        cell.textLabel?.textColor = .black
+        
         if(filter == "Calling") {
             cell.textLabel?.text = "\(self.items[indexPath.section][indexPath.row].LastName) \(self.items[indexPath.section][indexPath.row].FirstName) - \(self.items[indexPath.section][indexPath.row].Calling)"
         }
